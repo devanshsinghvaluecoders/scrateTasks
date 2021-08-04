2f5509161f49e82338907f39901f9fb9584fc811
devansh <devanshsingh5656>
2021-08-04 14:55:49 +0530

    test

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require("express");
+
+var dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+var app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello to devansah");
+});
+
+//login api
+
+app.listen(process.env.PORT || 3090, () => {
+  console.log(`connection succesfull  ${process.env.PORT}`);
+});
